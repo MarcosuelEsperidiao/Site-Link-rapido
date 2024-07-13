2c4aebd9c4d5acd61cf89f3096c0d37ef9b58ab6
MarcosuelEsperidiao <107445335+MarcosuelEsperidiao@users.noreply.github.com>
2024-07-13 01:40:21 -0300

    Selected service

--- Appoiment/scripts/script.js
+++ Appoiment/scripts/script.js
@@ -15,75 +15,79 @@ Array.from(icons).forEach(icon => {
 });
 
 function toggleIcon(icon, index) {
-  var services = JSON.parse(localStorage.getItem('services')) || []; 
-  var selectedServices = JSON.parse(localStorage.getItem('selectedServices')) || [];
-  var selectedService = services[index];
+    var services = JSON.parse(localStorage.getItem('services')) || [];
+    var selectedService = JSON.parse(localStorage.getItem('selectedService'));
+    var selectedService = services[index];
 
-  if (index === -1) {
-    console.error('Serviço não encontrado na lista de selecionados.');
-    return;
-}
+    if (index === -1) {
+        console.error('Serviço não encontrado na lista de selecionados.');
+        return;
+    }
 
-  if (icon.classList.contains("fa-plus")) {
-    // Se for o ícone de adição, troca para o ícone de verificação
-    icon.classList.remove("fa-plus", "selected");
-    icon.classList.add("fa-check");
+    // Desmarca todos os ícones antes de marcar o novo ícone
+    Array.from(icons).forEach(icon => {
+        icon.classList.remove("fa-check", "selected");
+        icon.classList.add("fa-plus");
+    });
 
-    // Adiciona o serviço selecionado ao array de serviços selecionados
-    selectedServices.push(selectedService);
-  } else {
-    // Se for o ícone de verificação, troca para o ícone de adição
-    icon.classList.remove("fa-check");
-    icon.classList.add("fa-plus", "selected");
-    // Remove o serviço selecionado do array de serviços selecionados
-    if (selectedServices.length === 1) {
-      // Se houver apenas um serviço restante, limpa o array ao invés de removê-lo
-      selectedServices = [];
-  } else {
-      selectedServices.splice(index, 1);
-  }
-  }
+    // Limpa a lista de serviços selecionados
+    selectedServices = [];
 
-  // Atualiza a lista de serviços selecionados no localStorage
-  localStorage.setItem('selectedServices', JSON.stringify(selectedServices));
-  icon.classList.toggle("selected");
-  console.log(selectedServices);
+    if (icon.classList.contains("fa-plus")) {
+        // Se for o ícone de adição, troca para o ícone de verificação
+        icon.classList.remove("fa-plus");
+        icon.classList.add("fa-check", "selected");
+
+        // Adiciona o serviço selecionado ao array de serviços selecionados
+      //  selectedServices.push(selectedService);
+        localStorage.setItem('selectedService', JSON.stringify(selectedService));
+    } else {
+        // Se for o ícone de verificação, troca para o ícone de adição
+        icon.classList.remove("fa-check", "selected");
+        icon.classList.add("fa-plus");
+        // Remove o serviço selecionado do array de serviços selecionados
+    localStorage.removeItem('selectedService');
+    }
+
+    // Atualiza a lista de serviços selecionados no localStorage
+    //localStorage.setItem('selectedServices', JSON.stringify(selectedServices));
+    console.log(selectedService);
 }
 
 function goToWorkerList() {
-  var selectedServices = JSON.parse(localStorage.getItem('selectedServices')) || [];
-  console.log('Serviço selecionado:', selectedServices);
+    var selectedServices = JSON.parse(localStorage.getItem('selectedServices')) || [];
+    console.log('Serviço selecionado:', selectedServices);
 
-  if (selectedServices.length > 0) {
-    window.location.href = 'list-workers.html';
-  } else {
-    alert('Por favor, selecione um serviço antes de avançar.');
-  }
+    if (selectedServices.length > 0) {
+        window.location.href = 'list-workers.html';
+    } else {
+        alert('Por favor, selecione um serviço antes de avançar.');
+    }
 }
 
 fetch('http://127.0.0.1:5000/services')
     .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json(); // Converte a resposta para JSON
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Converte a resposta para JSON
     })
     .then(data => {
-      localStorage.setItem('services', JSON.stringify(data));
+        localStorage.setItem('services', JSON.stringify(data));
 
-      const container = document.getElementById('container-services');
-      container.innerHTML = data.map((service, index) => `
-        <div id="item-service" class="service">
-          <div class="item-container">
-              <i id="icon" class="fas fa-plus icon" onclick='toggleIcon(this, ${index})'></i>
-          </div>
-          <li>${service.name}<br>
-            <h3 class="time">${service.duration} minutos</h3> 
-            <h3 class="value">R$ ${service.value}</h3>
-          </li>
-        </div>
-      `).join('');
+        const container = document.getElementById('container-services');
+        container.innerHTML = data.map((service, index) => `
+            <div id="item-service" class="service">
+                <div class="item-container">
+                    <i id="icon" class="fas fa-plus icon" onclick='toggleIcon(this, ${index})'></i>
+                </div>
+                <li>${service.name}<br>
+                    <h3 class="time">${service.duration} minutos</h3> 
+                    <h3 class="value">R$ ${service.value}</h3>
+                </li>
+            </div>
+        `).join('');
     })
     .catch(error => {
-      console.error('Fetch error:', error);
+        console.error('Fetch error:', error);
     });
